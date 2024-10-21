1e285f106065f845c19a0ac2068bf635831291d2
Leonardo Almeida <138615007+leonardoalmeida750@users.noreply.github.com>
2024-10-21 20:59:51 -0300

    render_template

--- microblog/app.py
+++ microblog/app.py
@@ -1,17 +1,17 @@
 #python -m venv venv
 #.\venv\Scripts\activate
 #pip install flask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá Mundo!'
+    return render_template('index.html')
 
 @app.route('/contato')
 def contato():
-    return 'exemplo@email.com'
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run()
